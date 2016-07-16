73c8a17e3b15eb16ee43df4f5c5cf748cf74ba25
Daniel Lienert <daniel@lienert.cc>
2016-07-16 10:05:42 +0200

    TASK: Change BLZ to IBAN

--- ext_tables.php
+++ ext_tables.php
@@ -169,7 +169,6 @@ t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Accounting mo
 						'starttime' => 'starttime',
 						'endtime' => 'endtime',
 					),
-					'sortby' => 'title',
 					'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'Configuration/TCA/CostType.php',
 					'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Icons/tx_dlaccounting_domain_model_costtype.gif'
 				),
